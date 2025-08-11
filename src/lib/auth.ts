516dbe16a3f8cd549b49e17d33ea8ba3a94171cb
Caio Beniel <caio-beniel@users.noreply.github.com>
2025-08-11 18:12:46 -0300

    chore: add better auth setup

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,12 +1,13 @@
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
-import { db } from "@/db";
 
-const auth = betterAuth({
+import { db } from "../db";
+
+export const auth = betterAuth({
+  emailAndPassword: {
+    enabled: true,
+  },
   database: drizzleAdapter(db, {
     provider: "pg",
   }),
 });
-
-export { auth };
-export default auth;
