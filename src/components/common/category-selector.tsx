ef9aa3e6dad18faaebc630a4a140ef59a3d90e9a
Caio Beniel <caio-beniel@users.noreply.github.com>
2025-08-12 16:17:08 -0300

    feat: add category products page

--- src/components/common/category-selector.tsx
+++ src/components/common/category-selector.tsx
@@ -1,27 +1,45 @@
-import { categoryTable } from "@/db/schema";
+import Image from "next/image";
+import Link from "next/link";
 
-import { Button } from "../ui/button";
+import { productTable, productVariantTable } from "@/db/schema";
+import { formatCentsToBRL } from "@/helpers/money";
+import { cn } from "@/lib/utils";
 
-interface CategorySelectorProps {
-  categories: (typeof categoryTable.$inferSelect)[];
+interface ProductItemProps {
+  product: typeof productTable.$inferSelect & {
+    variants: (typeof productVariantTable.$inferSelect)[];
+  };
+  textContainerClassName?: string;
 }
 
-const CategorySelector = ({ categories }: CategorySelectorProps) => {
+const ProductItem = ({ product, textContainerClassName }: ProductItemProps) => {
+  const firstVariant = product.variants[0];
   return (
-    <div className="rounded-3xl bg-[#F4EFFF] p-6">
-      <div className="grid grid-cols-2 gap-3">
-        {categories.map((category) => (
-          <Button
-            key={category.id}
-            variant="ghost"
-            className="rounded-full bg-white text-xs font-semibold"
-          >
-            {category.name}
-          </Button>
-        ))}
+    <Link href="/" className="flex flex-col gap-4">
+      <Image
+        src={firstVariant.imageUrl}
+        alt={firstVariant.name}
+        sizes="100vw"
+        height={0}
+        width={0}
+        className="h-auto w-full rounded-3xl"
+      />
+      <div
+        className={cn(
+          "flex max-w-[200px] flex-col gap-1",
+          textContainerClassName,
+        )}
+      >
+        <p className="truncate text-sm font-medium">{product.name}</p>
+        <p className="text-muted-foreground truncate text-xs font-medium">
+          {product.description}
+        </p>
+        <p className="truncate text-sm font-semibold">
+          {formatCentsToBRL(firstVariant.priceInCents)}
+        </p>
       </div>
-    </div>
+    </Link>
   );
 };
 
-export default CategorySelector;
+export default ProductItem; 
